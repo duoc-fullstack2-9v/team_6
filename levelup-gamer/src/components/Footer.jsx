0661d00a05180e41e44fdde9fad1b81a6a77ed00
dcabrerap97 <dieg.cabrerap@duocuc.cl>
2025-11-21 19:09:26 -0300

    change: arreglos al footer

--- levelup-gamer/src/components/Footer.jsx
+++ levelup-gamer/src/components/Footer.jsx
@@ -1,7 +1,23 @@
 export default function Footer() {
   return (
-    <footer className="container">
-      <small>© {new Date().getFullYear()} Level-Up Gamer</small>
+    <footer className="footer">
+      <div className="footer-col">
+        <h4>Level-Up Gamer</h4>
+        <p>Tienda gamer para subir de nivel 🎮</p>
+      </div>
+
+      <div className="footer-col">
+        <h4>Secciones</h4>
+        <a href="/productos">Productos</a>
+        <a href="/login">Login</a>
+        <a href="/cart">Carrito</a>
+      </div>
+
+      <div className="footer-col">
+        <h4>Contacto</h4>
+        <p>Email: contacto@levelup.cl</p>
+        <p>Soporte: soporte@levelup.cl</p>
+      </div>
     </footer>
   );
 }
